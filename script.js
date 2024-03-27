2bdd1c0d18b54bf0b1fb799b48b3e47bb935f200
haxekhaex2 <haxekhaex2@gmail.com>
2024-03-27 18:57:40 -0400

    include assets

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var config = {
 	type: Phaser.AUTO,
 	width: 480,
 	height: 864,
+	backgroundColor: 0xffffff,
 	physics: {
 		default: "arcade",
 		arcade: {
@@ -18,69 +19,124 @@ var config = {
 
 /* Globals. */
 let game = new Phaser.Game(config);
-let background;
 let grid;
 let input;
 let platforms;
 let player;
 
 function preload(){
+	/* Load images. */
 	this.load.image("bug", "assets/bug.png");
 	this.load.image("friend", "assets/friend.png");
 	this.load.image("background", "assets/background.png");
+	
+	this.load.spritesheet("cube0", "assets/cube0.png", {frameWidth: 200, frameHeight: 200}); 
+	
+	/* Load audio. */
+	this.load.audio("jump", "assets/jump.wav");
+	this.load.audio("land", "assets/land.wav");
 }
 
 function create(){
+	/* Initialize animations. */
+	this.anims.create({
+		key: "cube0_default",
+		frames: this.anims.generateFrameNumbers("cube0", {start: 0, end: 1}),
+		frameRate: 4,
+		repeat: -1
+	});
+	
+	this.anims.create({
+		key: "cube0_held",
+		frames: this.anims.generateFrameNumbers("cube0", {start: 2, end: 3}),
+		frameRate: 2,
+		repeat: -1
+	});
+	
+	this.anims.create({
+		key: "cube0_held",
+		frames: [{key: "cube0", frame: 4}],
+		frameRate: 2,
+		repeat: -1
+	});
+	
+	/* Initialize input handler. */
 	input = this.input.keyboard.addKeys({
 		"SPACE": Phaser.Input.Keyboard.KeyCodes.SPACE,
-		"W":  Phaser.Input.Keyboard.KeyCodes.W,
-		"A":  Phaser.Input.Keyboard.KeyCodes.A,
+		"W": Phaser.Input.Keyboard.KeyCodes.W,
+		"A": Phaser.Input.Keyboard.KeyCodes.A,
 		"S": Phaser.Input.Keyboard.KeyCodes.S,
 		"D": Phaser.Input.Keyboard.KeyCodes.D
 	});
 	
-	platforms = this.physics.add.staticGroup();
+	platforms = this.physics.add.group();
+	
 	
 	/* Add background. */
-	background = this.add.tileSprite(0, 0, this.game.canvas.width, this.game.canvas.height, "background");
-	grid = this.add.grid(0, 0, 1000, 1000, 100, 100, 0x000000, 0, 0xff0000, 1);
+	grid = this.add.grid(0, 0, 1000, 1000, 100, 100, 0x000000, 0, 0xff0000, 0);
 	
-	/* Create platforms. */
-	let startingPlatform = platforms.create(0, 200, "friend");
+	/* Create starting platform. */
+	let startingPlatform = this.physics.add.staticImage(0, 200, "friend");
 	startingPlatform.setSize(this.game.canvas.width, 200);
 	startingPlatform.setDisplaySize(this.game.canvas.width, 200);
+	
+	/* Create platforms. */
 	for(let index = 0; index < 100; index++){
-		createPlatform(Phaser.Math.Between(-this.game.canvas.width / 2, this.game.canvas.width / 2), index * -100);
+		createPlatform(Phaser.Math.Between(-this.game.canvas.width / 2, this.game.canvas.width / 2), index * -200);
 	}
 	
+	/* Create player. */
 	player = this.physics.add.sprite(50, 0, "bug");
 	player.setDisplaySize(32, 32);
-	this.physics.add.collider(player, platforms);
+	this.physics.add.collider(player, startingPlatform);
+	this.physics.add.collider(player, platforms, null, platformCollision, this);
 }
 
 function update(time, delta){
 	/* Camera and background movement. */
 	this.cameras.main.setScroll(-this.game.canvas.width / 2, player.y - this.sys.game.canvas.height / 2);
-	background.setTilePosition(this.cameras.main.scrollX, this.cameras.main.scrollY);	
-	background.setPosition(this.cameras.main.scrollX + this.sys.game.canvas.width / 2, this.cameras.main.scrollY + this.sys.game.canvas.height / 2);
-	grid.setPosition(Math.round(background.x / grid.cellWidth) * grid.cellWidth, Math.round(background.y / grid.cellHeight) * grid.cellHeight);
 	
 	/* Player input. */
 	let direction = 0;
 	if(input.A.isDown) direction -= 1;
 	if(input.D.isDown) direction += 1;
-	if(Phaser.Input.Keyboard.JustDown(input.SPACE) && player.body.touching.down) player.setVelocityY(-1024);
-	if(input.S.isDown) player.setVelocityY(player.body.velocity.y + (delta / 1000) * 2048);
+	if(input.S.isDown) player.setVelocityY(player.body.velocity.y + (delta / 1000) * 4096);
 	player.setVelocityX(direction * 512);
 	
-	/* Keep player in bounds. */
+	if(Phaser.Input.Keyboard.JustDown(input.SPACE) && player.body.touching.down){
+		player.setVelocityY(-1024);
+		this.sound.play("jump");
+	}
+	
+	/* Update platforms. */
+	platforms.children.iterate((child) => {
+		if(child.body.touching.up){
+			child.anims.play("cube0_held", true);
+			if(!child.held) this.sound.play("land");
+			child.held = true;
+		}else{
+			child.anims.play("cube0_default", true);
+			child.held = false;
+		}
+	});
 }
 
 /* Utility functions. */
 function createPlatform(x, y){
 	const PLATFORM_WIDTH = 200;
 	const PLATFORM_HEIGHT = 20;
-	let platform = platforms.create(x, y, "friend");
+	const CUBE_WIDTH = 200;
+	const CUBE_HEIGHT = 200
+	let platform = platforms.create(x, y, "cube0");
 	platform.setSize(PLATFORM_WIDTH, PLATFORM_HEIGHT);
-	platform.setDisplaySize(PLATFORM_WIDTH, PLATFORM_HEIGHT);
+	platform.setDisplaySize(CUBE_WIDTH, CUBE_HEIGHT);
+	//platform.setDisplayOrigin(CUBE_WIDTH / 2, CUBE_HEIGHT / 4 + platform.body.halfHeight);
+	platform.setOffset(0, CUBE_HEIGHT / 4);
+	platform.body.moves = false;
+	platform.body.immovable = true;
+	if(Math.round(Phaser.Math.Between(0, 2)) === 0) platform.flipX = true;
+}
+
+function platformCollision(player, platform){
+	return(player.body.velocity.y > 0 && ! input.S.isDown);
 }
